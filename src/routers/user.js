5d14aa87a28fef54e93812d287b9bc054672d560
zhifanl <64334331+zhifanl@users.noreply.github.com>
2021-07-26 00:18:41 -0600

    .

--- src/routers/user.js
+++ src/routers/user.js
@@ -24,7 +24,7 @@ router.post('/users', async (req, res) => {
 
         res.status(201).send({ user, token })
     } catch (e) {
-        res.status(400).send()
+        res.status(400).send(e)
 
     }
 
